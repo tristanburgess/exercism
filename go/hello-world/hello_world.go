ca0fadb93ce581b9800291068b3776ee9d4eab7a
Tristan Burgess <tburgessdev@gmail.com>
2021-02-07 12:04:04 -0500

    go track: hello world and skeleton code for

---
+++ go/hello-world/hello_world.go
@@ -0,0 +1,11 @@
+/*
+Package greeting exports a single method HelloWorld
+which allows a caller to generate a greeting string.
+*/
+package greeting
+
+// HelloWorld generates a static greeting string that says
+// "Hello, World!" and return it to the caller.
+func HelloWorld() string {
+	return "Hello, World!"
+}
