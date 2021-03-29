a44e715ac8644c8df8490529c8ffbdf1451b4bf6
Tristan Burgess <tburgessdev@gmail.com>
2021-03-29 07:46:20 -0400

    js track: two fer refactor with default param

--- javascript/two-fer/two-fer.js
+++ javascript/two-fer/two-fer.js
@@ -1,8 +1,3 @@
-export const twoFer = (name) => {
-  const isString = typeof name === 'string' && Object.prototype.toString.call(name) === '[object String]'
-  if (!isString || !name) {
-    return 'One for you, one for me.'
-  }
-
+export const twoFer = (name = 'you') => {
   return `One for ${name}, one for me.`
 };
