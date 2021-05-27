755e149b001d555f5731b2755877c4dc25148cc3
Tristan Burgess <tburgessdev@gmail.com>
2021-05-26 21:27:57 -0400

    js track: rna transcription reduce and map/join solutions

--- javascript/rna-transcription/rna-transcription.js
+++ javascript/rna-transcription/rna-transcription.js
@@ -5,10 +5,25 @@ const complements = {
   "A": "U",
 }
 
+// Loop impl
+/*
 export const toRna = (dnaSequence) => {
   let rnaSequence = ""
   for (let n of dnaSequence) {
     rnaSequence += complements[n]
   }
   return rnaSequence
+};
+*/
+
+// Reduce impl
+/*
+export const toRna = (dnaSequence) => {
+  return [...dnaSequence].reduce((rnaSequence, n) => rnaSequence += complements[n], '')
+};
+*/
+
+// Map/Join impl
+export const toRna = (dnaSequence) => {
+  return [...dnaSequence].map((n) => complements[n]).join('')
 };
